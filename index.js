99c85306d51775b51787b8d5ede9cdb0c2c25069
Kevin Raoofi <nullindev@gmail.com>
2017-04-29 23:48:25 -0400

    Converted from tabs to spaces

--- index.js
+++ index.js
@@ -13,23 +13,24 @@ const sio = require('socket.io')(server);
 const pin = 4;
 
 function getPinData() {
-	return {pin: pin, state: rpio.read(pin)};
+  return {pin: pin, state: rpio.read(pin)};
 }
+
 
 rpio.init({mapping: 'gpio'});
 rpio.open(pin, rpio.INPUT);
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-	res.send('hello world!');
+  res.send('hello world!');
 });
 
 sio.on('connection', (socket) => {
-	socket.emit('news', { hello: 'world'});
-	socket.emit('sensor', getPinData());
-	socket.on('update', (data) => {
-		socket.emit('sensor', getPinData());
-	});
+  socket.emit('news', { hello: 'world'});
+  socket.emit('sensor', getPinData());
+  socket.on('update', (data) => {
+    socket.emit('sensor', getPinData());
+  });
 });
 
 server.listen(3000);
@@ -37,19 +38,18 @@ server.listen(3000);
 let dataLog = [];
 
 rpio.poll(pin, (e) => {
-	let p = getPinData();
-	let last = dataLog.slice(-1).pop();
+  let p = getPinData();
+  let last = dataLog.slice(-1).pop();
 
-	if(last && p.state === last.state) {
-		console.log("ignored event");
-		return;
-	}
+  if(last && p.state === last.state) {
+    console.log("ignored event");
+    return;
+  }
 
-	let data = {time: moment().valueOf(), state: p.state};
+  let data = {time: moment().valueOf(), state: p.state};
 
-	dataLog = [...dataLog, data];
+  dataLog = [...dataLog, data];
 
-	console.log(dataLog);
-	sio.emit('sensor', p);
+  console.log(dataLog);
+  sio.emit('sensor', p);
 });
-
